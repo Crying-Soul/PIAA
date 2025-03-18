5ccb5b7ff5e6fac60b13c4561b0ee0408e880516
Crying-Soul <grebnevegor2000@mail.ru>
2025-03-18 12:14:41 +0300

    update

--- lb4/kmp.py
+++ lb4/kmp.py
@@ -1,43 +1,48 @@
-from colorama import Fore, Back, Style, init
+from colorama import Fore, Back, Style, init # type: ignore
 init(autoreset=True)
+
+from colorama import Fore
 
 def compute_lps(pattern, verbose=False):
     """
     Вычисляет массив LPS (Longest Prefix Suffix).
-    LPS[i] хранит длину наибольшего собственного префикса, который является суффиксом строки pattern[:i+1].
+    LPS[i] хранит длину наибольшего собственного префикса, который является суффиксом pattern[:i+1].
     """
     m = len(pattern)
     lps = [0] * m
-    length = 0  
-    i = 1  
+    length = 0  # Длина предыдущего совпадающего префикса
+    i = 1  # Начинаем со второго символа
 
     if verbose:
         print(Fore.CYAN + "\n=== Вычисление LPS ===")
     
     while i < m:
         if verbose:
-            print(Fore.YELLOW + f"\nШаг {i}: Текущая длина префикса {length}. Пытаемся сравнить pattern[{i}] = {pattern[i]} с pattern[{length}] = {pattern[length]}")
-
-        if pattern[i] == pattern[length]:  
+            print(Fore.YELLOW + f"Шаг {i}: Текущая длина префикса {length}. Сравниваем pattern[{i}] = {pattern[i]} с pattern[{length}] = {pattern[length] if length < i else 'N/A'}")
+        
+        if pattern[i] == pattern[length]:
             length += 1
             lps[i] = length
             i += 1
             if verbose:
                 print(Fore.GREEN + f"  Совпадение! Устанавливаем lps[{i-1}] = {length}")
-        elif length:  
-            length = lps[length - 1]  
-            if verbose:
-                print(Fore.RED + f"  Несовпадение! Уменьшаем длину префикса до lps[{length}]")
         else:
-            lps[i] = 0  
-            i += 1
-            if verbose:
-                print(Fore.RED + f"  Несовпадение! Устанавливаем lps[{i-1}] = 0 и увеличиваем i")
-
+            if length != 0:
+                length = lps[length - 1]  # Откатываемся назад
+                if verbose:
+                    print(Fore.RED + f"  Несовпадение! Откатываемся к lps[{length}]")
+            else:
+                lps[i] = 0
+                i += 1
+                if verbose:
+                    print(Fore.RED + f"  Несовпадение! Устанавливаем lps[{i-1}] = 0")
+    
     if verbose:
         print(Fore.MAGENTA + f"\nИтоговый массив LPS: {lps}\n")
     
     return lps
+
+from colorama import Fore
 
 def kmp_search(text, pattern, verbose=False):
     """
@@ -47,52 +52,57 @@ def kmp_search(text, pattern, verbose=False):
     n, m = len(text), len(pattern)
     if m == 0:
         return list(range(n + 1))
-
-    lps = compute_lps(pattern, verbose)
+    
+    # Вычисляем массив LPS (без вывода промежуточных сообщений)
+    lps = compute_lps(pattern, verbose=False)
+    
     indices = []
-    i = j = 0  
-
+    i, j = 0, 0  # i - индекс в text, j - индекс в pattern
+    
     if verbose:
         print(Fore.CYAN + "\n=== Поиск KMP ===")
-
+    
     while i < n:
-        if verbose:
-            print(Fore.YELLOW + f"\nШаг {i}: Сравниваем text[{i}] = {text[i]} с pattern[{j}] = {pattern[j]}")
-        
-        if text[i] == pattern[j]:  
+        # Если символы совпадают, переходим к следующему символу
+        if text[i] == pattern[j]:
+            if verbose:
+                print(Fore.YELLOW + f"\nШаг {i}: Сравниваем text[{i}] = {text[i]} с pattern[{j}] = {pattern[j]}")
+                print(Fore.GREEN + f"  Совпадение! Переходим к следующему символу: i = {i+1}, j = {j+1}")
             i += 1
             j += 1
+            
+            # Если найдено полное совпадение, сохраняем индекс и продолжаем поиск
+            if j == m:
+                indices.append(i - j)
+                if verbose:
+                    print(Fore.GREEN + f"  => Найдено вхождение на индексе {i - j}")
+                j = lps[j - 1]
+        else:
+            # Если происходит несовпадение, выводим информацию и корректируем индекс pattern
             if verbose:
-                print(Fore.GREEN + f"  Совпадение! Переходим к следующему символу: i = {i}, j = {j}")
-
-        if j == m:  
-            indices.append(i - j)  
-            if verbose:
-                print(Fore.GREEN + f"  => Найдено вхождение на индексе {i - j}")
-            j = lps[j - 1]  
-
-        elif i < n and text[i] != pattern[j]:  
-            if j:
-                j = lps[j - 1]  
+                print(Fore.YELLOW + f"\nШаг {i}: Сравниваем text[{i}] = {text[i]} с pattern[{j}] = {pattern[j]}")
+            if j != 0:
+                j = lps[j - 1]
                 if verbose:
                     print(Fore.RED + f"  Несовпадение! Переходим на j = {j} согласно lps")
             else:
-                i += 1  
+                i += 1
                 if verbose:
                     print(Fore.RED + f"  Несовпадение! Увеличиваем i: i = {i}")
-
+    
     if verbose:
         print(Fore.MAGENTA + f"\nИтоговые индексы вхождений: {indices}")
     
     return indices
 
+
 from test_kmp import *
 if __name__ == "__main__":
-    pattern = pattern5
-    text = text5
+    pattern = "лилила"
+    text = "лилилось лилилась"
     
     verbose = True
 
     result = kmp_search(text, pattern, verbose)
 
-    print(",".join(map(str, result)) if result else -1)
+    # print(",".join(map(str, result)) if result else -1)
